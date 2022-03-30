152824bfb8745dfaa1304927e01d94c8666d1a03
Juan Carlos Valdés <idevkingos@gmail.com>
2022-03-29 21:59:00 -0300

    feat: configuracion de router outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
@@ -9,6 +10,7 @@ const routes: Routes = [
   },
   {
     path: '',
+    component: HomePageComponent,
     loadChildren: () =>
       import(`./modules/home/home.module`).then((m) => m.HomeModule),
   },
