d187ae5e92761921c9a107614d382ce8db7d49da
Juan Carlos Valdés <idevkingos@gmail.com>
2022-03-29 22:35:21 -0300

    feat: media player html and styles

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-media-player',
   templateUrl: './media-player.component.html',
-  styleUrls: ['./media-player.component.scss']
+  styleUrls: ['./media-player.component.scss'],
 })
 export class MediaPlayerComponent implements OnInit {
+  mockCover = {
+    cover:
+      'https://media.resources.festicket.com/image/300x300/center/top/filters:quality(70)/www/artists/4.jpg',
+    album: 'Gioli & Assia',
+    name: 'BEBE (Oficial)',
+  };
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
